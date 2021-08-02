ff8b9dedc93c6a6c3b17583ff6b5d247e8e95ba9
diluo5261 <343917586@qq.com>
2021-08-02 23:51:48 +0800

    单例设计模式,饿汉式三种

---
+++ 面试/单例设计模式.java
@@ -0,0 +1,48 @@
+class Bank{
+	
+	//1.私化类的构造器
+	private Bank(){
+		
+	}
+	
+	//2.内部创建类的对象
+	//4.要求此对象也必须声明为静态的
+	private static Bank instance = new Bank();
+	
+	//3.提供公共的静态的方法，返回类的对象
+	public static Bank getInstance(){
+		return instance;
+	}
+}
+
+
+class Order{
+	
+	//1.私化类的构造器
+	private Order(){
+		
+	}
+	
+	//2.声明当前类对象，没初始化
+	//4.此对象也必须声明为static的
+	private static Order instance = null;
+
+	static{
+		instance = new Order();
+ }
+	
+	//3.声明public、static的返回当前类对象的方法
+	public static Order getInstance(){
+		return instance;
+	}
+}
+
+class Order{
+	
+	//1.私化类的构造器
+	private Order(){
+		
+	}
+	
+	public static final Order instance = new Order();	
+}
