5bee9a96bae2138f720c9d02a26f18daf7e24204
diluo5261 <343917586@qq.com>
2021-09-29 13:55:46 +0800

    mybatis框架实现CRUD

--- mybatis/mybaits/ch01_hello_mybatis/src/main/java/com/dilo/dao/StudentDao.java
+++ mybatis/mybaits/ch01_hello_mybatis/src/main/java/com/dilo/dao/StudentDao.java
@@ -9,4 +9,13 @@ public interface StudentDao {
 
     //查询student表所有的数据
     public List<Student> selectStudents();
+
+    //插入方法
+
+    /**
+     * 插入方法
+     * @param student 表示要插入数据看的数据
+     * @return  表示执行insert 操作后,影响数据的行数
+     */
+    public int insertStudent(Student student);
 }
