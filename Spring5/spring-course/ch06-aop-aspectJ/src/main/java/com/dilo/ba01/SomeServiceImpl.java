23f479c46f39ec5fc1f546ae83eeb0ad1ffd9e0c
diluo5261 <343917586@qq.com>
2021-10-04 23:16:05 +0800

    spring 集合 mybatis

---
+++ Spring5/spring-course/ch06-aop-aspectJ/src/main/java/com/dilo/ba01/SomeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.dilo.ba01;
+
+public class SomeServiceImpl implements SomeService{
+
+    @Override
+    public void doSome(String name, Integer age) {
+        System.out.println("目标方法的 doSome");
+
+
+    }
+}
---
+++ Spring5/spring-course/ch07-spring-mybatis/src/main/java/com/dilo/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.dilo.service;
+
+import com.dilo.domain.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    int addStudent(Student student);
+
+    List<Student> queryStudents();
+}
