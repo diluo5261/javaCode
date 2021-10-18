e1d9c23669fbad37af11b0733a95286bbe151c77
diluo5261 <343917586@qq.com>
2021-10-18 23:22:45 +0800

    读写锁的使用,写锁降级为读锁

---
+++ 多线程/ThreadCourse/day10_8/src/main/java/com/dilo/readwrite/LockIncDes.java
@@ -0,0 +1,34 @@
+package com.dilo.readwrite;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class LockIncDes {
+
+
+//演示读写锁降级
+
+
+    public static void main(String[] args) {
+        //可重入读写锁对象
+        ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+        ReentrantReadWriteLock.ReadLock readLock = rwLock.readLock();//读锁
+        ReentrantReadWriteLock.WriteLock writeLock = rwLock.writeLock();//写锁
+
+        //锁降级
+        //2 获取读锁
+        readLock.lock();
+        System.out.println("---read");
+
+        //1 获取写锁
+        writeLock.lock();
+        System.out.println("atguigu");
+
+        //3 释放写锁
+        //writeLock.unlock();
+
+        //4 释放读锁
+        //readLock.unlock();
+    }
+}
+
+
