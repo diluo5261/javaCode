4c5893bcf4f10166271d26a47e6ba4d6f769ed26
diluo5261 <343917586@qq.com>
2022-01-08 23:55:34 +0800

    幸运的袋子

---
+++ 面试/每日一题/day14_计算天数_幸运的袋子/幸运的袋子.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        
+        for(int i =0;i<n;i++){
+            a[i] = scan.nextInt();
+        }
+        System.out.ptintln(n);
+    }
+    
+    public static int count(int[] a ,int n,int pos,int sum,int multi){
+        int count = 0;
+        f
+    }
+}
